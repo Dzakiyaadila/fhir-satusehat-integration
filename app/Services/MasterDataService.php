976b9de45e14f8f6c95f1ad75624536b0cbb018b
atikah <mac@macs-MacBook-Pro.local>
2026-05-24 16:31:29 +0700

    feat: hanya untuk stream2

--- app/Services/MasterDataService.php
+++ app/Services/MasterDataService.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Services; // Perhatikan huruf S besar
+namespace App\Services; 
 
 use Illuminate\Support\Facades\Http;
 
@@ -16,67 +16,5 @@ class MasterDataService
         $this->authService = $authService;
     }
 
-    /**
-     * Kriteria: Mencari IHS Number Pasien berdasarkan NIK
-     */
-    public function getPatientIhs(string $nik): array
-    {
-        $token = $this->authService->getAccessToken();
-        
-        $response = Http::withToken($token)->get($this->baseUrl . "/Patient", [
-            'identifier' => 'https://fhir.kemkes.go.id/id/nik|' . $nik
-        ]);
-
-        if ($response->status() === 401) {
-            $newToken = $this->authService->forceNewToken();
-            $response = Http::withToken($newToken)->get($this->baseUrl . "/Patient", [
-                'identifier' => 'https://fhir.kemkes.go.id/id/nik|' . $nik
-            ]);
-        }
-
-        if ($response->status() === 404 || empty($response->json()['entry'])) {
-            return [
-                'success' => false,
-                'message' => 'NIK Pasien tidak terdaftar di SATUSEHAT.',
-                'fallback_id' => '100000000001'
-            ];
-        }
-
-        return [
-            'success' => true,
-            'ihs_number' => $response->json()['entry'][0]['resource']['id']
-        ];
-    }
-
-    /**
-     * Kriteria: Mencari IHS Number Dokter (Practitioner) berdasarkan NIK
-     */
-    public function getPractitionerIhs(string $nik): array
-    {
-        $token = $this->authService->getAccessToken();
-
-        $response = Http::withToken($token)->get($this->baseUrl . "/Practitioner", [
-            'identifier' => 'https://fhir.kemkes.go.id/id/nik|' . $nik
-        ]);
-
-        if ($response->status() === 401) {
-            $newToken = $this->authService->forceNewToken();
-            $response = Http::withToken($newToken)->get($this->baseUrl . "/Practitioner", [
-                'identifier' => 'https://fhir.kemkes.go.id/id/nik|' . $nik
-            ]);
-        }
-
-        if ($response->status() === 404 || empty($response->json()['entry'])) {
-            return [
-                'success' => false,
-                'message' => 'NIK Dokter tidak terdaftar di SATUSEHAT.',
-                'fallback_id' => 'N10000001'
-            ];
-        }
-
-        return [
-            'success' => true,
-            'ihs_number' => $response->json()['entry'][0]['resource']['id']
-        ];
-    }
+    
 }
